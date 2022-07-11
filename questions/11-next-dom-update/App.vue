10d251f0f90f0cfd08281fa2d27d10184235d850
flippedround <734243792@qq.com>
2022-07-11 15:42:08 +0800

    feat: add test case for Q11

--- questions/11-next-dom-update/App.vue
+++ questions/11-next-dom-update/App.vue
@@ -3,7 +3,6 @@ import { ref } from "vue"
 
 const count = ref(0)
 const counter = ref(null)
-const textContent = ref("")
 
 function increment() {
   count.value++
@@ -12,9 +11,7 @@ function increment() {
    * DOM is not yet updated, how can we make sure that the DOM gets updated
    * Make the output be true
   */
-
-  textContent.value = counter.value.textContent
-  console.log(+textContent.value === 1)
+  console.log(+counter.value.textContent === 1)
 }
 </script>
 
--- questions/11-next-dom-update/index.test.ts
+++ questions/11-next-dom-update/index.test.ts
@@ -1,23 +1,23 @@
 import { mount } from "@vue/test-utils"
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, vi } from "vitest"
 
 import App from "./App.vue"
 
-interface AppType {
-  count: number
-  textContent: number
-  increment: () => Promise<void>
-}
-
 describe("next-dom-update", () => {
   it("should work'", async() => {
+    let printLog = ''
+    console.log = vi.fn(
+      (log: string) => {
+        printLog = log?.toString()?.trim()
+      })
     const wrapper = mount(App)
 
-    expect(wrapper.text()).toMatchInlineSnapshot('"0"')
+    expect(wrapper.text()).toMatchInlineSnapshot("\"0\"")
+
     const button = wrapper.find("button")
     await button.trigger("click")
 
     expect(wrapper.text()).toMatchInlineSnapshot('"1"')
-    expect((wrapper.vm as unknown as AppType).textContent).toMatchInlineSnapshot('"1"')
+    expect(printLog).toMatchInlineSnapshot('"true"')
   })
 })
